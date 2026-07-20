3c38439ba6a13e448f4094a22268fef9af74eba3
Gustavoinho <126987095+gustavoinho@users.noreply.github.com>
2026-07-19 22:11:20 -0300

    nome e anexo

--- routes/agendamentos.js
+++ routes/agendamentos.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage,
   limits:{
-    fileSize: 5 * 1024 * 1024
+    fileSize: 10 * 1024 * 1024
   },
   fileFilter:(req,file,cb)=>{
 
